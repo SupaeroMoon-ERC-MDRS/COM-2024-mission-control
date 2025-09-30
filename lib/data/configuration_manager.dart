a2c9b624a3fa62d2f18e573995fd0fa577d6cc80
davidgmolnar <92262848+davidgmolnar@users.noreply.github.com>
2025-09-30 14:42:26 +0200

    Prepared pre launch reconfig

--- lib/data/configuration_manager.dart
+++ lib/data/configuration_manager.dart
@@ -1,8 +1,30 @@
+import 'dart:io';
+
+import 'package:supaeromoon_mission_control/data/discovery.dart';
+import 'package:supaeromoon_mission_control/io/file_system.dart';
+
 abstract class ConfigurationManager{
   static bool reconfig(){
-    // TODO put remote path in gs config
-    // TODO put netcode path in gs config
-    // TODO put dbc path in gs config
+    final String path = Platform.isWindows ? "${DPath.groundStationFolder}Release/Local/" : Platform.isLinux ? "${DPath.groundStationFolder}bundle/Local/" : throw Exception("Unsupported platform");
+    final Map data = FileSystem.tryLoadMapFromLocalSync(path, "SESSION");
+
+    final String dbcPath = ""; // TODO
+    final String netCodePath = ""; // TODO
+    final String remotePath = ""; // TODO
+
+    if(data.containsKey("dbcPaths") && data["dbcPaths"] is List){
+      if(!data["dbcPaths"].cast<String>().contains(dbcPath)){
+        data["dbcPaths"].add(dbcPath);
+      }
+    }
+    else{
+      data["dbcPaths"] = [dbcPath];
+    }
+
+    data["netCodePath"] = netCodePath;
+    data["remotePath"] = remotePath;
+
+    FileSystem.trySaveMapToLocalSync(path, "SESSION", data);
     return true;
   }
 }
